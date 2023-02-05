8bca395a150ac282856010dcc74a2fbf3078f35d
Daniyal Mahmood <daniyalmahmood123@gmail.com>
2023-02-06 01:23:49 +0530

    Add registration form 	modified:   package-lock.json 	new file:   public/style.css 	modified:   routes/index.js 	new file:   views/register.html

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,19 @@ const express = require('express');
 
 const router = express.Router();
 
+// router.use(bodyParser.urlencoded({ extended: false }))
 router.get('/', (req, res) => {
     res.send('Welcome to the home page');
 });
+router.get("/register", (req, res) => {
+    res.sendFile("register.html", { root: "views" });
+})
+router.post("/register", (req, res) => {
+    let resData = {
+        serverData: req.body,
+    };
+    console.log(resData);
+    res.status(200).send({success:true});
 
+})
 module.exports = router;
